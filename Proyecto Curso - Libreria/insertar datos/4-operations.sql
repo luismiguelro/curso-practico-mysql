9b1570a6a50b0a43055eab5c27ce094234d61601
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-07-19 04:58:27 -0500

    [add] datos operations y clase join

---
+++ Proyecto Curso - Libreria/insertar datos/4-operations.sql
@@ -0,0 +1,8 @@
+INSERT INTO operations (book_id, client_id, type, finished) VALUES
+	(12,34,'sell',1),
+	(54,87,'lend',0),
+	(3,14,'sell',1),
+	(1,54,'sell',1),
+	(12,81,'lend',1),
+	(12,81,'sell',1),
+	(87,29,'sell',1);
---
+++ Proyecto Curso - Libreria/recursos/16-join.sql
@@ -0,0 +1,39 @@
+-- Listar todos los autores con ID entre 1 y 5 con los filtro mayor y menor igual
+SELECT * FROM authors WHERE author_id > 0 AND author_id <= 5;
+
+-- Listar todos los autores con ID entre 1 y 5 con el filtro BETWEEN
+SELECT * FROM authors WHERE author_id BETWEEN 1 AND 5;
+
+-- Listar los libros con filtro de author_id entre 1 y 5
+SELECT book_id, author_id, title FROM books WHERE author_id BETWEEN 1 AND 5;
+
+-- Listar nombre y titulo de libros mediante el JOIN de las tablas books y authors
+SELECT b.book_id, a.name, a.author_id, b.title
+FROM books AS b
+JOIN authors AS a
+  ON a.author_id = b.author_id
+WHERE a.author_id BETWEEN 1 AND 5;
+
+-- Listar transactions con detalle de nombre, titulo y tipo. Con los filtro genero = F y tipo = Vendido.
+-- Haciendo join entre transactions, books y clients.
+SELECT b.title,c.name,c.gender,op.type
+FROM operations AS op
+JOIN books AS b
+  ON op.book_id = b.book_id
+JOIN clients AS c
+  ON op.client_id = c.client_id
+WHERE c.gender = 'F'
+  AND op.type = 'sell';
+
+-- Listar transactions con detalle de nombre, titulo, autoor y tipo. Con los filtro genero = M y de tipo = Vendido y Devuelto.
+-- Haciendo join entre transactions, books, clients y authors.
+  SELECT b.title,c.name,c.gender,op.type
+FROM operations AS op
+JOIN books AS b
+  ON op.book_id = b.book_id
+JOIN clients AS c
+  ON op.client_id = c.client_id
+  JOIN authors AS a
+  ON b.author_id = a.author_id
+WHERE c.gender = 'M'
+  AND op.type IN ('sell', 'lend');
