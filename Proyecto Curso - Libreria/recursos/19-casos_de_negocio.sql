0eb7aca758bb3ea0c8ee4a8ba0541bd6cf08d44c
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-07-28 05:04:17 -0500

    [add] consultas v.2

--- Proyecto Curso - Libreria/recursos/19-casos_de_negocio.sql
+++ Proyecto Curso - Libreria/recursos/19-casos_de_negocio.sql
@@ -24,10 +24,32 @@ GROUP BY a.nationality
 
 
 -- 4. ¿Cual es el promedio/desviacion standard del precio de los libros
+use platzi_operation;
 SELECT 
-    AVG(price) AS avg_price, STDDEV(price) AS stddev_price
+    a.nationality,
+    AVG(b.price) AS promedio,
+    STDDEV(b.price) AS std
 FROM
-    books;
+    books AS b
+        JOIN
+    authors AS a ON a.author_id = b.author_id
+GROUP BY a.nationality
+ORDER BY promedio DESC;
+
+-- ¿Cuál es el promedio/desviación standard del precio de libros por nacionalidad?
+-- Agrupar por la columna pivot
+
+SELECT 
+    a.nationality,
+    COUNT(b.book_id) AS libros,
+    AVG(b.price) AS promedio,
+    STDDEV(b.price) AS std
+FROM
+    books AS b
+        JOIN
+    authors AS a ON a.author_id = b.author_id
+GROUP BY a.nationality
+ORDER BY libros DESC;
 
 -- 5. cual es el precio minimo/maximo de un libro?
 SELECT 
@@ -51,4 +73,17 @@ FROM
     ORDER BY price ASC
     LIMIT 1) AS title_book_min;
 
+-- ¿Cuál es el precio máximo/mínimo de un libro(y el nombre)? por nacionalidad
+SELECT 
+    b.title, a.nationality, MAX(price), MIN(price)
+FROM
+    books AS b
+        JOIN
+    authors AS a ON a.author_id = b.author_id
+GROUP BY  b.title,a.nationality;
 
+
+-- Generar precios aleatorios
+<UPDATE books 
+  SET price = FLOOR(RAND()*(35-10+1))+10
+  WHERE book_id between 1 and 198 >
