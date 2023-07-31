19b6c73162d94b8a4ec76ab6bfacd46e02c4e199
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-07-31 05:43:46 -0500

    [add] bd proyecto twitter

---
+++ Proyecto - Base datos Twitter/create_tables_twitter.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE twitter_db;
+
+USE twitter_db;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT,
+    user_handle VARCHAR(50) NOT NULL UNIQUE,
+    email_address VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    phonenumber CHAR(10) UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (user_id)
+);
