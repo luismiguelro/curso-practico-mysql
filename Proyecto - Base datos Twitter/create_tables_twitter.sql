14dc260e8e68956fa1c74f8f70418656f3592822
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-08-02 21:46:23 -0500

    [add] tabla followers y datos

--- Proyecto - Base datos Twitter/create_tables_twitter.sql
+++ Proyecto - Base datos Twitter/create_tables_twitter.sql
@@ -2,6 +2,7 @@ CREATE DATABASE twitter_db;
 
 USE twitter_db;
 
+-- Users
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT,
     user_handle VARCHAR(50) NOT NULL UNIQUE,
@@ -12,3 +13,19 @@ CREATE TABLE users (
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (user_id)
 );
+
+-- Followers
+CREATE TABLE followers (
+    follower_id INT NOT NULL,
+    following_id INT NOT NULL,
+    FOREIGN KEY (follower_id)
+        REFERENCES users (user_id),
+    FOREIGN KEY (follower_id)
+        REFERENCES users (user_id),
+    PRIMARY KEY (follower_id , following_id)
+);
+-- Version 8 añadir constrains para hacer checks
+-- Validar que una misma persona se siga asi misma
+ALTER TABLE followers
+add constraint check_follower_id
+check(follower_id <> following_id);
