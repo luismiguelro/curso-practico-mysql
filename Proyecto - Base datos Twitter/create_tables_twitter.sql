b8160f4458f97014a433ac625e0923917e872b68
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-08-09 22:03:41 -0500

    [add] trigger contador de seguidores

--- Proyecto - Base datos Twitter/create_tables_twitter.sql
+++ Proyecto - Base datos Twitter/create_tables_twitter.sql
@@ -10,6 +10,7 @@ CREATE TABLE users (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     phonenumber CHAR(10) UNIQUE,
+    follower_count INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY (user_id)
 );
---
+++ Proyecto - Base datos Twitter/triggers.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE TRIGGER increase_follower_count
+AFTER INSERT ON followers
+FOR EACH ROW
+BEGIN
+    -- Cuando se inserta una fila en "followers", incrementa el contador de seguidores en la tabla "users"
+    UPDATE users
+    SET follower_count = follower_count + 1
+    WHERE user_id = NEW.following_id;
+END $$
+DELIMITER ;
