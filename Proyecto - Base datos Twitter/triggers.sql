72dc96da0a9333b51965547f2d25aa98c239c46f
Luis Miguel Rodriguez <luismiguelrodriguez33@gmail.com>
2023-08-09 22:07:41 -0500

    [add] trigger dejar de seguir

--- Proyecto - Base datos Twitter/triggers.sql
+++ Proyecto - Base datos Twitter/triggers.sql
@@ -9,3 +9,17 @@ BEGIN
     WHERE user_id = NEW.following_id;
 END $$
 DELIMITER ;
+
+_________________________________________________________________________
+
+DELIMITER $$
+CREATE TRIGGER decrease_follower_count
+AFTER DELETE ON followers
+FOR EACH ROW
+BEGIN
+    -- Cuando se elimina una fila en "followers", incrementa el contador de seguidores en la tabla "users"
+    UPDATE users
+    SET follower_count = follower_count - 1
+    WHERE user_id = NEW.following_id;
+END $$
+DELIMITER ;
